24184e345b409091e9f91a8bca5483a8a605e322
robel <robelwon75@gmail.com>
2024-02-13 13:42:28 +0300

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,10 @@ class Auth:
     
     def authorization_header(self, request=None) -> str:
         """ Request Flask Object """
-        return None
+        if request == None or 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
     
     def current_user(self, request=None) -> TypeVar('User'):
         """ flask request object"""

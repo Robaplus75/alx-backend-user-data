02001837469c94bed92448efaebd1d4c3d0a7e55
robel <robelwon75@gmail.com>
2024-02-14 19:24:13 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,14 @@ class BasicAuth(Auth):
         elif 'Basic' not in authorization_h:
             return None
         return authorization_h[6:]
+    
+    def decode_base64_authorization_header(self, b64_auth_header: str) -> str:
+        """ Decodes base64 authorization """
+        if b64_auth_header is None or not isinstance(b64_auth_header, str):
+            return None
+        try:
+            b64 = base64.b64decode(b64_auth_header)
+            b64_decode = b64.decode('utf-8')
+        except Exception:
+            return None
+        return b64_decode

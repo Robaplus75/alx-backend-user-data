fb840478e4d8791edcb75d47cd60fe7890c85b1e
robel <robelwon75@gmail.com>
2024-02-14 12:48:22 +0300

    task 7: doc fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ extract the authorization header """
         if authorization_header is None or not isinstance(authorization_header, str):
             return None
         elif 'Basic' not in authorization_header:

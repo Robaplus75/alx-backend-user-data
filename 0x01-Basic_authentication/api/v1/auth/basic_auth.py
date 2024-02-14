0df58a18f5851fe355bd633161749e4c2feae16e
robel <robelwon75@gmail.com>
2024-02-14 12:52:04 +0300

    task 7: pycodestyle fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,10 +6,10 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Auth class """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_h: str) -> str:
         """ extract the authorization header """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_h is None or not isinstance(authorization_h, str):
             return None
-        elif 'Basic' not in authorization_header:
+        elif 'Basic' not in authorization_h:
             return None
-        return authorization_header[6:] 
+        return authorization_h[6:]

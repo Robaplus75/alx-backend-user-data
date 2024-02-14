0646f2d88c5ecde00b9d4dfe8415b57f02e27286
robel <robelwon75@gmail.com>
2024-02-14 12:45:23 +0300

    task 7: var name fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        if authorization_header is None or not isinstance(auth_header, str):
+        if authorization_header is None or not isinstance(authorization_header, str):
             return None
         elif 'Basic' not in authorization_header:
             return None

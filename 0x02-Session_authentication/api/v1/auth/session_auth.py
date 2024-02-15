838f1072b0993aebbbcca0c682fabc7fa68a2422
robel <robelwon75@gmail.com>
2024-02-15 20:41:51 +0300

    task 2: pycoestyle fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,7 @@ from models.user import User
 class SessionAuth(Auth):
     """ Session  auth class"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ Create Session """
         if user_id is None or not isinstance(user_id, str):
@@ -18,17 +19,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ get session _id """
-        if session_id is None or not isinstance(session_id, str):
-            return None
-        return self.user_id_by_session_id.get(session_id, None)
-    def current_user(self, request=None):
-        """ return current user"""
-        cookie = self.session_cookie(request)
-        session_user_id = self.user_id_for_session_id(cookie)
-        user_id = User.get(session_user_id)
-        return user_id
+
     def destroy_session(self, request=None):
         """ deletes session """
         cookie_data = self.session_cookie(request)
@@ -38,3 +29,16 @@ class SessionAuth(Auth):
             return False
         del self.user_id_by_session_id[cookie_data]
         return True
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ get session _id """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """ return current user"""
+        cookie = self.session_cookie(request)
+        session_user_id = self.user_id_for_session_id(cookie)
+        user_id = User.get(session_user_id)
+        return user_id

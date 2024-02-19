305162f35c9432e540958b61aa41c0d0a74db87b
robel <robelwon75@gmail.com>
2024-02-19 21:02:00 +0300

    task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -30,6 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
     
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to the database """
         user = User()
@@ -40,3 +43,28 @@ class DB:
         self._session.commit()
 
         return user
+
+
+    def find_user_by(self, **kwargs) -> User:
+        """returns the first row filtered by the kwarg"""
+        user_keys = ['id', 'email', 'hashed_password', 'session_id',
+                     'reset_token']
+        for key in kwargs.keys():
+            if key not in user_keys:
+                raise InvalidRequestError
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates the user's info"""
+        user_keys = ['id', 'email', 'hashed_password', 'session_id',
+                     'reset_token']
+        user_to_update = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key in user_keys:
+                setattr(user_to_update, key, value)
+            else:
+                raise ValueError
+        self._session.commit()

21bf6a5aec6ea0f0e32d38975157a1161b39fd25
robel <robelwon75@gmail.com>
2024-02-21 19:35:57 +0300

    task 0: another import bug fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ contains User Model """
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()

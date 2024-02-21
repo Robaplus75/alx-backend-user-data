c3987fb3540a5cd6aacc7d5e613739bfb2f1cd52
robel <robelwon75@gmail.com>
2024-02-21 19:25:55 +0300

    bug fix

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Authentication methods """
+
+import bcrypt
+
+def _hash_password(password: str):
+    """ for hashing the password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

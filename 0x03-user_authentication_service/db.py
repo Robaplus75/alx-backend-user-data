1879160aa67bc55abbb634c4bdbd898b50bd7cd3
robel <robelwon75@gmail.com>
2024-02-21 19:44:54 +0300

    task 1:

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+"""DB module"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -35,9 +35,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to the database """
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
+        user = User(email=email, hashed_password=hashed_password)
 
         self._session.add(user)
         self._session.commit()
